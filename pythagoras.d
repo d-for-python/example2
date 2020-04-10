2c49f6b9fde1177e5004997130e37e46db0f2688
Victor Porton <porton@narod.ru>
2020-04-11 02:18:42 +0300

    Create pythagoras.d

---
+++ pythagoras.d
@@ -0,0 +1,12 @@
+import std.math;
+
+double pythagoras(double leg1, double leg2) {
+  return sqrt(leg1*leg1, leg2*leg2);
+}
+
+void main() {
+  double leg1 = 10.2;
+  double leg2 = 7.31;
+  double hypotenuse = pythagoras(leg1, leg2);
+  writeln("Hypotenuse = ", hypotenuse);
+}
